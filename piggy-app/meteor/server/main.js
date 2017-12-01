2d7659490abfbfeb732127e2f8f5d72c5772479b
margaritayong <lyong@sva.edu>
2017-11-30 22:39:48 -0500

    updated Serial port

--- piggy-app/meteor/server/main.js
+++ piggy-app/meteor/server/main.js
@@ -7,7 +7,7 @@ const rgbHex = require('rgb-hex');
  
 const Readline = SerialPort.parsers.Readline;
 const parser = new Readline();
-var port = new SerialPort('/dev/cu.usbmodem1421', {
+var port = new SerialPort('/dev/cu.usbmodem1431', {
   baudRate: 9600
 });
 port.pipe(parser);
